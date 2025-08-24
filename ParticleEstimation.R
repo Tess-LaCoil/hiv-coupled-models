c1b6351490193af248acc53b55ee869ea013bf61
Tess-LaCoil <tesslacoil@protonmail.com>
2025-08-24 11:16:35 +1000

    Start of odin2/dust/monty based estimation.'

---
+++ ParticleEstimation.R
@@ -0,0 +1,22 @@
+if(FALSE){
+  install.packages(
+    "dust2",
+    repos = c("https://mrc-ide.r-universe.dev", 
+              "https://cloud.r-project.org"))
+  
+  install.packages(
+    "monty",
+    repos = c("https://mrc-ide.r-universe.dev", 
+              "https://cloud.r-project.org"))
+  
+  install.packages(
+    "odin2",
+    repos = c("https://mrc-ide.r-universe.dev", 
+              "https://cloud.r-project.org"))
+}
+
+library(dust2)
+library(monty)
+library(odin2)
+
+
